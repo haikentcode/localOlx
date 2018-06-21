196dd9629eeade8a3d47f48734a1a41e6cb64559
haikentcode <hiteshnitj16@gmail.com>
2018-06-21 20:20:47 +0530

    update to new python and django version

--- localOlx/wsgi.py
+++ localOlx/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for localOlxNew project.
+WSGI config for localOlx project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localOlxNew.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localOlx.settings')
 
 application = get_wsgi_application()
